4520c06ae8bcbf0ee041f3b55273bda4495d7a22
Henry Jetmundsen <hjetmundsen@protonmail.com>
2026-03-13 15:35:26 -0700

    Minor tweaks

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,9 +1,7 @@
 return {
 	"ibhagwan/fzf-lua",
-	-- optional for icon support
-	-- dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- or if using mini.icons/mini.nvim
 	dependencies = { "nvim-mini/mini.icons" },
+	lazy = false,
 	keys = {
 		{
 			"<leader>ff",
